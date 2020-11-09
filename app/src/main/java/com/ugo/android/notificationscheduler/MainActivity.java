2ac9e7ff235588973c07a9ae1a5adb82144927f6
Ugo119 <onuoha.ugochukwu@yahoo.com>
2020-11-09 15:50:10 +0100

    Made changes to codebase

--- app/src/main/java/com/ugo/android/notificationscheduler/MainActivity.java
+++ app/src/main/java/com/ugo/android/notificationscheduler/MainActivity.java
@@ -8,7 +8,9 @@ import android.content.ComponentName;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int JOB_ID = 0;
     private Switch deviceIdleSwitch;
     private Switch deviceChargingSwitch;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +27,47 @@ public class MainActivity extends AppCompatActivity {
 
         deviceIdleSwitch = findViewById(R.id.idleSwitch);
         deviceChargingSwitch = findViewById(R.id.chargingSwitch);
+        seekBar = findViewById(R.id.seekBar);
+
+        final TextView seekBarProgress = findViewById(R.id.seekBarProgress);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress > 0) {
+                    seekBarProgress.setText(progress + " s");
+                } else {
+                    seekBarProgress.setText("Not Set");
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+    /**
+     * onClick method that schedules the jobs based on the parameters set.
+     */
 
     public void scheduleJob(View view) {
         RadioGroup networkOptions = findViewById(R.id.networkOptions);
         jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
 
+        int seekBarInteger = seekBar.getProgress();
+        boolean seekBarSet = seekBarInteger > 0;
+
         //Get the selected network ID and save it in an integer variable
         int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
-
         int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+
         switch (selectedNetworkID) {
             case R.id.noNetwork:
                 selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
@@ -62,6 +96,10 @@ public class MainActivity extends AppCompatActivity {
         //Show a Toast message, letting user know the job was scheduled
         Toast.makeText(this, "Job Scheduled, job will run when " +
                 "the constraints are met.", Toast.LENGTH_LONG).show();
+
+        if (seekBarSet) {
+            builder.setOverrideDeadline(seekBarInteger * 1000);
+        }
     }
 
     public void cancelJobs(View view) {
